b293b0f51e1447fd12cc0cf5fdf67c34d06dfe39
Jessie Link <jlink@twitter.com>
2019-09-25 11:33:10 +0100

    Added function to calculate area of a Circle. Just foolin' around.

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"github.com/madtypist/stringutil"
 	"time"
 )
+
+func areaOfCircle(r float64) float64 {
+	return math.Pi * math.Pow(r, 2)	
+}
 
 func main() {
 	// need to Seed rand to make it truly (somewhat) random
@@ -13,8 +18,9 @@ func main() {
 
 	fmt.Println("Hello, world.")
 	fmt.Println(stringutil.Reverse("Hello, Go!"))
-	fmt.Println("My favorite number is ", rand.Intn(20))
+	fmt.Println("My favorite number is", rand.Intn(20))
 
 	// Printf fun
 	fmt.Printf("I've got %g problems, but finding the area of a circle ain't one.\n", math.Pi)
+	fmt.Println(areaOfCircle(5))
 }
