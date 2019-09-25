fc3fb571a1f59617bacbcf92176b5a1ef6f8e3ef
Jessie Link <jlink@twitter.com>
2019-09-25 10:48:41 +0100

    Added in time library. Added randomized Seed to number generator

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
-
+	"math/rand"
 	"github.com/madtypist/stringutil"
+	"time"
 )
 
 func main() {
+	// need to Seed rand to make it truly (somewhat) random
+	rand.Seed(time.Now().UnixNano())
+
 	fmt.Println("Hello, world.")
 	fmt.Println(stringutil.Reverse("Hello, Go!"))
+	fmt.Println("My favorite number is ", rand.Intn(20))
 }
