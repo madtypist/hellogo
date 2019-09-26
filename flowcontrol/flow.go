b374af1b9c7330c2024991d1ed5eec37a81071fa
Jessie Link <jlink@twitter.com>
2019-09-26 10:24:31 +0100

    Different ways to articulate loops

--- flowcontrol/flow.go
+++ flowcontrol/flow.go
@@ -8,9 +8,22 @@ func main() {
 	// a simple for loop
 	sum := 0
 	for i := 0; i < 10; i++ {
-		fmt.Println("Sum + ", i, " =", sum)
 		sum += i
+		fmt.Println("Sum + ", i, " =", sum)
 	}
 
-	fmt.Println("Final sum =", sum)
+
+	// a different loop leaving out explicit declaration of init and post statements
+	sum = 1
+	for ; sum < 1000; {
+		sum += sum
+	}
+	fmt.Println(sum)
+
+	// same idea, but now done demonstrating that 'for' is 'while' in Go
+	sum = 1
+	for sum < 1000 {
+		sum += sum
+		fmt.Println(sum)
+	}
 }
